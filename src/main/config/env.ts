8f5fe4a0b3b9d2acf578e9dbdeaba0f6b808a501
Joao Eduardo Ribeiro Rocha <113138169+JoaoEduardoRRocha@users.noreply.github.com>
2025-01-05 21:38:21 -0300

    chore: update nome discord secret

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   port: process.env.PORT || 5050,
   clientID: process.env.CLIENT_ID || '',
-  discordSecret: process.env.JWT || '',
+  discordSecret: process.env.DISCORD_SECRET || '',
   openAiSecret: process.env.API_KEY_OPENAI || ''
 }
